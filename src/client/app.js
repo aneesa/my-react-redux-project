7adaa9dd74ca498fccbdd370ef28f1dc9ceea3d8
Aneesa Awaludin <a_aneesa@yahoo.com>
2017-01-27 11:10:24 +0800

    Add Redux

--- src/client/app.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-console */
-import 'babel-polyfill';
-import Dog from '../shared/dog';
-
-const browserToby = new Dog('Browser Toby');
-document.querySelector('.app').innerText = browserToby.bark();
